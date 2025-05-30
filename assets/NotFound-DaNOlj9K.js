882934c6c926aac00b3f103775d10682f99678bc
eguneys <eguneys@users.noreply.github.com>
2025-05-30 00:28:41 +0000

    deploy: 6b2858a23ab2d2a2ac28073f3c1720621780bcb9

--- assets/NotFound-DaNOlj9K.js
+++
@@ -1,1 +0,0 @@
-import{t as o,i as r,c as e}from"./index-BpXD8XUH.js";import{A as n}from"./components-CJm56cbL.js";var a=o("<main class=not-found><p>404 Not Found");function m(){return(()=>{var t=a();return t.firstChild,r(t,e(n,{href:"/",children:"Back to Home"}),null),t})()}export{m as default};
