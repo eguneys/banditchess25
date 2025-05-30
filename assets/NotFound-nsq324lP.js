88194763de9527d51bf0b4196b65f9daa30412c8
eguneys <eguneys@users.noreply.github.com>
2025-05-30 00:18:46 +0000

    deploy: c811870ae64438382f7654381bfbfde22d87a996

---
+++ assets/NotFound-nsq324lP.js
@@ -0,0 +1,1 @@
+import{t as o,i as r,c as e}from"./index-BCUrnEPb.js";import{A as n}from"./components-C4oCXvmI.js";var a=o("<main class=not-found><p>404 Not Found");function m(){return(()=>{var t=a();return t.firstChild,r(t,e(n,{href:"/",children:"Back to Home"}),null),t})()}export{m as default};
